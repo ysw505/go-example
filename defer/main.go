bad3560682965fa502a5ec32e9c1dab27b13cc53
ysw <ysw5055@gmail.com>
2018-01-22 23:43:27 +0900

    add defer example

--- defer/main.go
+++ defer/main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Defer Example")
+	fmt.Println("Defer is stack")
+
+	defer func() {
+		fmt.Println("defer 0")
+	}()
+
+	foo()
+}
+
+
+func foo() {
+	defer func() {
+		fmt.Println("defer 1")
+	}()
+
+	defer func() {
+		fmt.Println("defer 2")
+	}()
+
+	defer func() {
+		fmt.Println("defer 3")
+	}()
+
+	defer func() {
+		fmt.Println("defer 4")
+	}()
 }
